75c2ed84f028844294f8c01c61423407ee87f44a
Holistic <paulofreire.si@outlook.com>
2021-04-01 01:07:07 -0300

    watching bojack

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const spawn = (father, child) => document.querySelector(father).appendChild(chil
 
 const hero = document.createElement('img');
 const hr = hero.style;
-hero.src = '/home/mvsyd/Documents/Holistic95.github.io/holistic.jpeg';
+hero.src = 'https://user-images.githubusercontent.com/78622030/113242055-66a08180-9286-11eb-9589-f534dbad9bfe.jpeg';
 hr.backgroundPosition = 'center';
 hr.backgroundSize = 'cover';
 hr.position = 'relative';
@@ -16,10 +16,10 @@ const header = document.createElement('header');
 const hd = header.style;
 hd.textAlign = 'center';
 hd.position = 'absolute';
-hd.top = '80%';
+hd.top = '77%';
 hd.left = '50%';
-hd.transform = 'translate (-50%, -50%)'
 hd.color = 'black';
+hd.fontFamily = ''
 header.innerText = 'Holistic';
 
-// spawn('body', header);
+spawn('body', header);
