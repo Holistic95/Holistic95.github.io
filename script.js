3c68c2810e6e7dcecd2962574c3d925ff016acbc
Holistic <paulofreire.si@outlook.com>
2021-04-03 00:39:23 -0300

    just testing around

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-document.body.style.backgroundColor = 'rgb(233, 233, 233)';
 const spawn = (father, child) => document.querySelector(father).appendChild(child);
+
+const body = document.body.style;
+body.backgroundColor = 'rgb(233, 233, 233)';
 
 const hero = document.createElement('img');
 const hr = hero.style;
 hero.src = 'https://user-images.githubusercontent.com/78622030/113242055-66a08180-9286-11eb-9589-f534dbad9bfe.jpeg';
-hr.backgroundPosition = 'center';
-hr.backgroundSize = 'cover';
-hr.position = 'relative';
-hr.backgroundRepeat = 'no-repeat';
-hr.width = '100%';
-hr.height = 'auto';
+hr.backgroundSize = '50%';
+hr.display = 'flex';
+hr.flexDirection = 'column';
+hr.justifyContent = 'center';
+hr.alignItems = 'center';
+hr.width = '100%'; 
+hr.maxHeight = '50%';
 spawn('body', hero);
+
+const div = document.createElement('div');
+spawn('body', div);
 
 const header = document.createElement('header');
 const hd = header.style;
 hd.textAlign = 'center';
 hd.position = 'absolute';
-hd.top = '77%';
+hd.top = '80%';
 hd.left = '50%';
 hd.color = 'black';
-hd.fontFamily = ''
-header.innerText = 'Holistic';
+hd.transform = 'translate(-50%, -50%)';
+hd.fontFamily = 'Arial';
+hd.fontSize = '50px';
+// header.innerText = 'Holistic';
 
-spawn('body', header);
+document.querySelector('div').appendChild(header);
